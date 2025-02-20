dd12e950c2d4eb77b4cc9ca9176d6ec35e1834ec
TQV <tranquocviet226@Viet-MacBook-M3-Pro.local>
2025-02-20 15:48:54 +0700

    Corodomo Maintenance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen justify-center items-center p-24 w-screen">
       <p className="bg-gradient-to-r from-[#8770ff] to-[#ff7eda] bg-clip-text text-transparent text-5xl text-center">
-        Corodomo is under maintenance – We'll be back soon!
+        Corodomo is under maintenance – We&apos;ll be back soon!
       </p>
       <p className="text-center mt-8">Corodomo hiện đang bảo trì – Chúng tôi sẽ sớm quay lại! 🚀</p>
     </main>
