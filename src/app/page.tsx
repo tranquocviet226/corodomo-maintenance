3264e89b509e5cf970cf6bd093af2416a9ccf6aa
TQV <tranquocviet226@Viet-MacBook-M3-Pro.local>
2025-02-20 16:01:32 +0700

    Corodomo Maintenance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen justify-center items-center p-24 w-screen">
-      <p className="bg-gradient-to-r from-[#8770ff] to-[#ff7eda] bg-clip-text text-transparent text-5xl text-center">
+      <p className="bg-gradient-to-r from-[#8770ff] to-[#ff7eda] bg-clip-text text-transparent text-5xl text-center font-bold">
         Corodomo is under maintenance – We&apos;ll be back soon!
       </p>
       <p className="text-center mt-8">Corodomo hiện đang bảo trì – Chúng tôi sẽ sớm quay lại! 🚀</p>
